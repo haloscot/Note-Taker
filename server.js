b2eec23ee8622797d089c13a2f02a10ca11f3693
haloscot <hannahlorrainescott@gmail.com>
2020-02-08 03:13:04 -0500

    simplified server.js. Added routes folder with apiRoutes.js and htmlRoutes.js

--- server.js
+++ server.js
@@ -9,43 +9,13 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Data
-var counter = 0;
-var ID = function(){
-    counter++;
-    this.name = counter;
-}
-var notes = [
-  {
-    routeID: ID,
-    note: "this is a note",
-  },
-];
+// Routes saved in /routes
 
-// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-// Returns all saved notes as JSON
-app.get("/api/notes", function(req, res) {
-  return res.json(notes);
-});
 
-// Recieves a new note to save on  request body, 
-// adds to the `db.json` file, and returns new note to client.
-app.post("/api/notes", function(req, res) {
-  var newNote = req.body;
-
-  console.log(newNote);
-
-  notes.push(newNote);
-
-  res.json(newNote);
-});
-
+// Listener
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
-});
-
-// Recieves a query paramter containing the id of a note to delete.
-app.delete("/api/notes/:id", function(req, res) {
-
 });
